d17e0f2ef8b3f6e2489395a74755412a96aa2461
IamBobo <abdulsamad.arilewola@gmail.com>
2026-08-10 18:20:35 +0100

    Correct import syntax and update BrowserRouter
    Fix import statements and remove basename from BrowserRouter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
